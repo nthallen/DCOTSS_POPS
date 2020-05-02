938695bb4edf3860885bc4cc2c66125f7582dcb5
Norton Allen <allen@huarp.harvard.edu>
2020-05-01 21:39:34 -0400

    Adapt uDACS_init() to dual boards

--- TM/uDACS.cmd
+++ TM/uDACS.cmd
@@ -4,7 +4,9 @@
   #include "uDACS_cmd.h"
 
   subbuspp *uDA;
+  subbuspp *uDB;
   bool uDACS_A_present = false;
+  bool uDACS_B_present = false;
   
   void uDACS_fail(bool on) {
     if (uDACS_A_present) {
@@ -12,17 +14,28 @@
     }
   }
   
-  void uDACS_init() {
-    msg(0, "uDACS_init()");
-    uDA = new subbuspp("uDACS_A", "serusb");
-    msg(0, "uDA->load()");
-    int subfunc = uDA->load();
+  /**
+   * @param A true for uDACS A, false for uDACS B
+   */
+  void uDACS_init(bool A) {
+    char which = A ? 'A' : 'B';
+    msg(0, "uDACS_init(%c)", which);
+    subbuspp *uD;
+    uD = new subbuspp(A ? "uDACS_A" : "uDACS_B", "serusb");
+    msg(0, "uD%c->load()", which);
+    int subfunc = uD->load();
     if (subfunc == 0) {
       msg(2, "subbus load() failed");
     } else {
-      uDACS_A_present = true;
       if (subfunc != 9 && subfunc != 14)
         msg(2, "Expected Subfunction 9 or 14 for uDACS, was %d", subfunc);
+      if (A) {
+        uDACS_A_present = true;
+        uDA = uD;
+      } else {
+        uDACS_B_present = true;
+        uDB = uD;
+      }
     }
   }
   #endif
