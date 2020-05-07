f63fe2e1aeb9059c595b504b256ad1fee5857806
Norton Allen <allen@huarp.harvard.edu>
2020-05-07 13:19:43 -0400

    Extract more channels

--- eng/gdpopsdacsb.m
+++ eng/gdpopsdacsb.m
@@ -1,5 +1,5 @@
 function fig = gdpopsdacsb(varargin);
 % gdpopsdacsb(...)
 % uDACS B
-ffig = ne_group(varargin,'uDACS B','pdpopsdacsbdac','pdpopsdacsbais','pdpopsdacsbv','pdpopsdacsbs','pdpopsdacsbaics','pdpopsdacsbain','pdpopsdacsbai');
+ffig = ne_group(varargin,'uDACS B','pdpopsdacsbpv','pdpopsdacsbais','pdpopsdacsbrt','pdpopsdacsbs','pdpopsdacsbaics','pdpopsdacsbain','pdpopsdacsbai');
 if nargout > 0 fig = ffig; end
