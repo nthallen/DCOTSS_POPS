e233fd1e06039b363a3fc1b6ffe6310acb8aef16
Norton Allen <allen@huarp.harvard.edu>
2020-05-19 19:03:33 -0400

    Fix temp resolution to work around TMC Cal generation bug

--- eng/gdpopsa.m
+++ eng/gdpopsa.m
@@ -1,5 +1,5 @@
 function fig = gdpopsa(varargin);
 % gdpopsa(...)
 % Alicat
-ffig = ne_group(varargin,'Alicat','pdpopsaccm','pdpopsasccm');
+ffig = ne_group(varargin,'Alicat','pdpopsasccm','pdpopsaccm');
 if nargout > 0 fig = ffig; end
