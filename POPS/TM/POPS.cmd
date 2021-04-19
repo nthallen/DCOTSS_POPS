2ac59aa2037756998ea056348fdb40626d5d624c
Norton Allen <allen@huarp.harvard.edu>
2021-04-19 17:27:23 -0400

    Add POPS Server Shutdown Force

--- POPS/TM/POPS.cmd
+++ POPS/TM/POPS.cmd
@@ -4,5 +4,6 @@
   : POPS Start * { if_POPS.Turf("B\n"); }
   : POPS Shutdown * { if_POPS.Turf("S\n"); }
   : POPS Server Shutdown * { if_POPS.Turf("E\n"); }
+  : POPS Server Shutdown Force * { if_POPS.Turf("F\n"); }
   : POPS Driver Quit * { if_POPS.Turf("Q\n"); }
   ;
