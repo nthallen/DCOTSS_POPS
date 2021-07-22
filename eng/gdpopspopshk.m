55d3bbc38381ddc752cdef205eccdbae891d49b2
Norton Allen <allen@huarp.harvard.edu>
2021-07-22 15:45:20 -0500

    Add surface area, volume density calcs

---
+++ eng/gdpopspopshk.m
@@ -0,0 +1,5 @@
+function fig = gdpopspopshk(varargin);
+% gdpopspopshk(...)
+% POPS HK
+ffig = ne_group(varargin,'POPS HK','pdpopspopshkstd','pdpopspopshkldt','pdpopspopshkldm','pdpopspopshkb','pdpopspopshks','pdpopspopshkstale');
+if nargout > 0 fig = ffig; end
