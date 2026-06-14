9f790413392a1a288a9bc6dda9ae9ce8c4826de5
Norton Allen <norton_allen@fastmail.net>
2026-06-14 16:49:51 -0400

    Cleanup displays, extractions and plots

--- eng/gdpopsa.m
+++ eng/gdpopsa.m
@@ -1,5 +1,5 @@
 function fig = gdpopsa(varargin)
 % gdpopsa(...)
 % Algo
-ffig = ne_group(varargin,'Algo','pdpopsasws','pdpopsag','pdpopsaf','pdpopsamf','pdpopsalp','pdpopsaip','pdpopsasa');
+ffig = ne_group(varargin,'Algo','pdpopsasws','pdpopsam','pdpopsag','pdpopsaf','pdpopsamf','pdpopsalp','pdpopsaip','pdpopsasa','pdpopsasp');
 if nargout > 0 fig = ffig; end
