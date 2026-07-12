de6fe92f1cbca48f62ac1c89cd77594141eb8c15
Norton Allen <norton_allen@fastmail.net>
2026-07-12 16:20:34 -0400

    PGS receiving completed

---
+++ eng/gdpgsmm.m
@@ -0,0 +1,5 @@
+function fig = gdpgsmm(varargin)
+% gdpgsmm(...)
+% MM
+ffig = ne_group(varargin,'MM','pdpgsmmmode','pdpgsmmcmd','pdpgsmmstatus','pdpgsmmrt','pdpgsmmv','pdpgsmmf');
+if nargout > 0 fig = ffig; end
